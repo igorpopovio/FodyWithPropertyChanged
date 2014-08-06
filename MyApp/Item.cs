7768dfafb63c53b3174afc9915dd3ad69daab474
Igor Popov <igor@igorpopov.io>
2014-08-06 16:20:52 +0300

    Add Fody
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- MyApp/Item.cs
+++ MyApp/Item.cs
@@ -1,33 +1,17 @@
+using PropertyChanged;
+
 namespace MyApp
 {
-    public class Item : ObservableObject
+    [ImplementPropertyChanged]
+    public class Item
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
         public Item()
         {
             Name = "Testing Item";
             Description = "Just an item used for testing";
-        }
-
-        private string _name;
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                _name = value;
-                OnPropertyChanged("Name");
-            }
-        }
-
-        private string _description;
-        public string Description
-        {
-            get { return _description; }
-            set
-            {
-                _description = value;
-                OnPropertyChanged("Description");
-            }
         }
     }
 }
